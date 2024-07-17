945e1446dcdc5272f5b7a49e286dbd84ab2b9672
kanadome <tsuchiyakeita2025@gmail.com>
2024-07-17 22:33:53 +0900

    データベースに接続するコードを追加

--- backend/app/main.go
+++ backend/app/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -10,6 +14,25 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// データベース接続
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(db:3306)/%s",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_DATABASE"),
+	))
+	if err != nil {
+		fmt.Println("Failed to connect to database:", err)
+		return
+	}
+	defer db.Close()
+
+	// データベース接続確認
+	if err := db.Ping(); err != nil {
+		fmt.Println("Database is not reachable:", err)
+		return
+	}
+	fmt.Println("Successfully connected to database")
+
 	http.HandleFunc("/", helloHandler)
 	fmt.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
