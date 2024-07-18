f9f8a2c0cae28504b6322d61bd4f67fe9a9b8516
ardririy <hynnkgw27@gmail.com>
2024-07-18 21:39:38 +0900

    構造体とデータベースの作成

--- backend/app/main.go
+++ backend/app/main.go
@@ -1,37 +1,60 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
-	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+type User struct {
+	ID                uint   `gorm:"primary_key"`
+	UserName          string `gorm:"unique;not null"`
+	EncryptedPassword string `gorm:"not null"`
+}
+
+type Subscription struct {
+	ID          uint      `gorm:"primary_key"`
+	UserId      uint      `gorm:"not null"`
+	AppName     string    `gorm:"not null"`
+	Price       int       `gorm:"not null"`
+	Interval    string    `gorm:"not null"`
+	PayMentDate time.Time `sql:"not null;type:date"`
+	Period      int
+	StartDate   time.Time
+}
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world")
 }
 
 func main() {
-	// データベース接続
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(db:3306)/%s",
+	// DBのセットアップ
+	dsn := fmt.Sprintf("%s:%s@tcp(db:3306)/%s",
 		os.Getenv("MYSQL_USER"),
 		os.Getenv("MYSQL_PASSWORD"),
 		os.Getenv("MYSQL_DATABASE"),
-	))
+	)
+	// db, err := sql.Open("mysql", dsn)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
 	if err != nil {
 		fmt.Println("Failed to connect to database:", err)
 		return
 	}
-	defer db.Close()
+	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Subscription{})
+	fmt.Println("Successfully connected to database")
 
-	// データベース接続確認
-	if err := db.Ping(); err != nil {
-		fmt.Println("Database is not reachable:", err)
+	// ユーザを登録してみる
+	res := db.Create(&User{UserName: "test", EncryptedPassword: "password"})
+	if res.Error != nil {
+		fmt.Println("Failed to create user:", res.Error)
 		return
 	}
-	fmt.Println("Successfully connected to database")
 
 	http.HandleFunc("/", helloHandler)
 	fmt.Println("Starting server on :8080")
