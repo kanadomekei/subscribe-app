a0bf98948579b8f50099132c949df36455e370e6
kanadome <tsuchiyakeita2025@gmail.com>
2024-07-20 14:34:21 +0900

    ec2が一つだけ作られるように変更

--- infra/terraform/modules/instances/main.tf
+++ infra/terraform/modules/instances/main.tf
@@ -14,18 +14,18 @@ resource "aws_instance" "dev_server" {
   }
 }
 
-resource "aws_instance" "prod_server" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  subnet_id              = var.subnet2_id
-  key_name               = var.key_name
-  vpc_security_group_ids = [var.security_group_id]
+# resource "aws_instance" "prod_server" {
+#   ami                    = var.ami
+#   instance_type          = var.instance_type
+#   subnet_id              = var.subnet2_id
+#   key_name               = var.key_name
+#   vpc_security_group_ids = [var.security_group_id]
 
-  root_block_device {
-    volume_size = 10
-  }
+#   root_block_device {
+#     volume_size = 10
+#   }
 
-  tags = {
-    Name = "prod-server"
-  }
-}
+#   tags = {
+#     Name = "prod-server"
+#   }
+# }
